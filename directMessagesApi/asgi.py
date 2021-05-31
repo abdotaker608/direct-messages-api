bdc8d881d5919034df08746f25934aa9392c9fb9
Eldababa <abdotaker607@gmail.com>
2021-05-31 18:26:08 +0200

    Fixed App Registry Issue

--- directMessagesApi/asgi.py
+++ directMessagesApi/asgi.py
@@ -10,14 +10,16 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'directMessagesApi.settings')
+django_asgi_app = get_asgi_application()
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from websocket.routing import routes
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'directMessagesApi.settings')
-
 application = ProtocolTypeRouter({
-    'http': get_asgi_application(),
+    'http': django_asgi_app,
     'websocket': AllowedHostsOriginValidator(
         URLRouter(
             routes
