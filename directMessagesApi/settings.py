0b675e16b625272260a2309c95ec3361006df38e
Eldababa <abdotaker607@gmail.com>
2021-05-31 18:43:25 +0200

    Redis URL

--- directMessagesApi/settings.py
+++ directMessagesApi/settings.py
@@ -82,7 +82,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            'hosts': [(os.getenv('REDIS_URL'), )]
+            'hosts': [(os.getenv('REDIS_URL'))]
         }
     }
 }
