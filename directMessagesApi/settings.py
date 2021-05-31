e011f98ff11206eda44ac7b421c6de33ca4b57a4
Eldababa <abdotaker607@gmail.com>
2021-05-31 18:29:31 +0200

    Updated Requirements.txt

--- directMessagesApi/settings.py
+++ directMessagesApi/settings.py
@@ -146,7 +146,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR.joinpath('static')
-STATICFILES_STORAGE = "whitenose.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Media files
 
